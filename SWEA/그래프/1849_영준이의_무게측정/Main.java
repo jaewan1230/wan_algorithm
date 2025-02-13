b5f0aeceb6d9d2d66937e672427ea6b35cd46a95
jaewan1230 <wjp1230@naver.com>
2025-02-13 22:47:13 +0900

    Solve: SWEA 1849

--- SWEA/그래프/1849_영준이의_무게측정/Main.java
+++ SWEA/그래프/1849_영준이의_무게측정/Main.java
@@ -1,87 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+	static int[] parent, value;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc < T; tc++) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(String.format("#%d ", tc));
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
 
-			Graph g = new Graph();
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= T; tc++) {
+			sb.append(String.format("#%d ", tc));
+			String[] input = br.readLine().split(" ");
+			int N = Integer.parseInt(input[0]);
+			int M = Integer.parseInt(input[1]);
+			parent = new int[N + 1];
+			value = new int[N + 1];
 			for (int i = 1; i <= N; i++)
-				g.addNode(i);
+				parent[i] = i;
 
 			for (int i = 0; i < M; i++) {
-				String[] inStr = br.readLine().split(" ");
-				if (inStr[0].charAt(0) == '!') {
-					g.addEdge(g.getNode(Integer.parseInt(inStr[1])), g.getNode(Integer.parseInt(inStr[2])), Integer.parseInt(inStr[3]));
+				input = br.readLine().split(" ");
+				int a = Integer.parseInt(input[1]);
+				int b = Integer.parseInt(input[2]);
+				if (input[0].equals("!")) {
+					int w = Integer.parseInt(input[3]);
+					union(a, b, w);
 				}
-				else if (inStr[0].charAt(0) == '?') {
-					dfs(Integer.parseInt(inStr[1]), Integer.parseInt(inStr[2]), 0);
+				if (input[0].equals("?")) {
+					int parentA = find(a), parentB = find(b);
+					if (parentA != parentB)
+						sb.append("UNKNOWN ");
+					else
+						sb.append(String.format("%d ", value[b] - value[a]));
 				}
 			}
+			sb.append('\n');
 		}
+		System.out.println(sb.toString());
 	}
 
-	static void dfs(int cur, int dest, int res) {
-		if(cur==dest) {
-			
+	static int find(int n) {
+		int root = n;
+		Stack<Integer> stack = new Stack<>();
+		while (root != parent[root]) {
+			stack.push(root);
+			root = parent[root];
 		}
-	}
-	static class Graph {
-		Map<Integer, Node> nodes;
-
-		public Graph() {
-			this.nodes = new HashMap<>();
-		}
-
-		void addNode(int no) {
-			nodes.put(no, new Node(no));
+		int node;
+		while (!stack.isEmpty()) {
+			node = stack.pop();
+			value[node] += value[parent[node]];
+			parent[node] = root;
 		}
 
-		void addEdge(Node source, Node destination, int weight) {
-			source.addEdge(new Edge(source, destination, weight));
-			destination.addEdge(new Edge(destination, source, -weight));
-		}
-
-		Node getNode(int no) {
-			return nodes.get(no);
-		}
+		return root;
 	}
 
-	static class Node {
-		List<Edge> edges;
-		int no;
-
-		public Node(int no) {
-			this.no = no;
-			edges = new ArrayList<>();
-		}
-
-		void addEdge(Edge e) {
-			edges.add(e);
-		}
-
-	}
-
-	static class Edge {
-		Node source, dest;
-		int weight;
-
-		public Edge(Node source, Node dest, int weight) {
-			this.source = source;
-			this.dest = dest;
-			this.weight = weight;
+	static void union(int a, int b, int w) {
+		int rootA = find(a);
+		int rootB = find(b);
+		if (rootA != rootB) {
+			value[rootB] = value[a] + w - value[b];
+			parent[rootB] = rootA;
 		}
 	}
 }
